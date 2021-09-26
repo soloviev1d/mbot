25ad517430b199b35d084b91cd7425d502c640b0
soloviev1d <91020666+soloviev1d@users.noreply.github.com>
2021-09-26 16:58:59 +0000

    Переписал -play на стриминг, добавил когу, добавил -playurl, теперь боту не нужно выходить чтобы играть другую песню.

--- main.py
+++ main.py
@@ -4,14 +4,15 @@
 import os
 import discord
 from discord.ext import commands
-from discord import FFmpegPCMAudio
-import youtube_dl
-import json
-import urllib.parse, urllib.request, re
 import keep_alive
+import music
 
+cogs = [music]
 my_secret = os.environ['token']
 client = commands.Bot(command_prefix = '-')
+
+for i in range(len(cogs)):
+  cogs[i].setup(client)
 
 @client.event
 async def on_ready():
@@ -19,47 +20,8 @@ async def on_ready():
   print('~~~~~~~~~~~~~~~~~~')
 
 @client.command(pass_context = True)
-async def leave(ctx):
-  if(ctx.voice_client):
-    await ctx.guild.voice_client.disconnect()
-    await ctx.send('I left the vc')
-  else:
-    await ctx.send('I am not in a vc!')
-
-@client.command(pass_context = True)
 async def amogus(ctx):
   await ctx.send(file=discord.File('gus.gif'))
-
-@client.command(pass_context = True)
-async def play(ctx, *,search):
-  if(ctx.author.voice):
-    channel = ctx.message.author.voice.channel
-    voice = await channel.connect()
-    query_string = urllib.parse.urlencode({
-      'search_query': search
-    })
-    htm_content = urllib.request.urlopen(
-      'https://www.youtube.com/results?' + query_string
-    )
-    search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode()) #1000-7?
-    ydl_opts = {
-      'format': 'bestaudio/best', 'postprocessors':[{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-      }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-      ydl.download(['https://www.youtube.com/watch?v=' + search_results[0]])
-    for file in os.listdir("./"):
-      if file.endswith(".mp3"):
-        os.rename(file, "song.mp3")
-    source = FFmpegPCMAudio('song.mp3')
-    player = voice.play(source)
-  else:
-    await ctx.send('You are not in a vc!')
-
-
 
 keep_alive.keep_alive()
 client.run(my_secret)
---
+++ music.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+import urllib.parse, urllib.request, re
+
+class music(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+  
+  @commands.command()
+  async def playurl(self, ctx, url):
+    if ctx.author.voice is None:
+      await ctx.send("You are not in a vc")
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+      YDL_OPTIONS = {'format':"bestaudio"}
+      vc = ctx.voice_client
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+          info = ydl.extract_info(url, download=False)
+          url2 = info['formats'][0]['url']
+          source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+          vc.play(source)
+    else:
+      ctx.voice_client.stop()
+      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+      YDL_OPTIONS = {'format':"bestaudio"}
+      vc = ctx.voice_client
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+          info = ydl.extract_info(url, download=False)
+          url2 = info['formats'][0]['url']
+          source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+          vc.play(source)
+  @commands.command()
+  async def leave(self, ctx):
+    await ctx.voice_client.disconnect()
+    await ctx.channel.send("I left vc")
+  
+  @commands.command()
+  async def pause(self, ctx):
+    ctx.voice_client.pause()
+    await ctx.channel.send("Paused")
+
+  @commands.command()
+  async def resume(self, ctx):
+    ctx.voice_client.resume()
+    await ctx.channel.send("Resumed")
+  
+  @commands.command()
+  async def play(self,ctx, *, search):
+    if ctx.author.voice is None:
+      await ctx.send("You are not in a vc")
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+      await voice_channel.connect()
+      query_string = urllib.parse.urlencode({
+      'search_query': search
+      })
+      htm_content = urllib.request.urlopen(
+      'https://www.youtube.com/results?' + query_string
+      )
+      search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode()) #1000-7?
+      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+      YDL_OPTIONS = {'format':"bestaudio"}
+      vc = ctx.voice_client
+      url = 'https://www.youtube.com/watch?v=' + search_results[0]
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+          info = ydl.extract_info(url, download=False)
+          url2 = info['formats'][0]['url']
+          source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+          vc.play(source)
+    else:
+      ctx.voice_client.stop()
+      query_string = urllib.parse.urlencode({
+      'search_query': search
+      })
+      htm_content = urllib.request.urlopen(
+      'https://www.youtube.com/results?' + query_string
+      )
+      search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode()) #1000-7?
+      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+      YDL_OPTIONS = {'format':"bestaudio"}
+      vc = ctx.voice_client
+      url = 'https://www.youtube.com/watch?v=' + search_results[0]
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+          info = ydl.extract_info(url, download=False)
+          url2 = info['formats'][0]['url']
+          source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+          vc.play(source)
+
+def setup(client):
+  client.add_cog(music(client))
